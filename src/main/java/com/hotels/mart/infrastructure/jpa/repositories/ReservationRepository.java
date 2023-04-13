64cae4cd36a08d64ad9a567010f42db23091ea3d
Tito Nova <tnova@mysanitas.com>
2023-04-13 17:41:18 -0500

    [hu240] Se agrgar filtro fechas sql new

--- src/main/java/com/hotels/mart/infrastructure/jpa/repositories/ReservationRepository.java
+++ src/main/java/com/hotels/mart/infrastructure/jpa/repositories/ReservationRepository.java
@@ -17,8 +17,10 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
       "WHERE (:userId is null or r.user_id.id = :userId) " +
       "AND (:roomId is null or r.room_id.id = :roomId) " +
       "AND (:reservationStateId is null or r.reservation_state_id.id = :reservationStateId) " +
-      "AND (:checkInDate is null or r.check_in_date >= :checkInDate) " +
-      "AND (:checkOutDate is null or r.check_out_date >= :checkOutDate)")
+      "AND ((:checkInDate is null and :checkOutDate is null) " +
+      "   or (:checkInDate is null and r.check_out_date >= :checkOutDate) " +
+      "   or (:checkOutDate is null and r.check_in_date >= :checkInDate) " +
+      "   or (r.check_in_date <= :checkOutDate and r.check_out_date >= :checkInDate))")
   List<Reservation> findByCriteria(@Param("userId") Long userId,
       @Param("roomId") Long roomId,
       @Param("reservationStateId") Long reservationStateId,
@@ -26,14 +28,3 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
       @Param("checkOutDate") LocalDateTime checkOutDate);
 
 }
-
-
-// @Query("SELECT r FROM Reservation r " +
-// "WHERE (:userId IS NULL OR r.user_id.id = :userId) " +
-// "AND (:roomId IS NULL OR r.room_id.id = :roomId) " +
-// "AND (:reservationStateId IS NULL OR r.reservation_state_id.id =
-// :reservationStateId) " +
-// "AND (:checkInDate IS NULL OR r.checkInDate >= :checkInDateStart AND
-// r.checkInDate <= :checkInDateEnd) " +
-// "AND (:checkOutDate IS NULL OR r.checkOutDate >= :checkOutDateStart AND
-// r.checkOutDate <= :checkOutDateEnd)")
