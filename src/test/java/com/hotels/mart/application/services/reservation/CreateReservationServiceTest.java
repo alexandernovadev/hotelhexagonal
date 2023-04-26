8096b597f08990975fc402954ffc0213e5ffc73f
dani101820 <111008893+dani101820@users.noreply.github.com>
2023-04-25 19:16:29 -0500

    testing service

---
+++ src/test/java/com/hotels/mart/application/services/reservation/CreateReservationServiceTest.java
@@ -0,0 +1,11 @@
+package com.hotels.mart.application.services.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class CreateReservationServiceTest {
+    @Test
+    void testCreateReservation() {
+        
+    }
+}
