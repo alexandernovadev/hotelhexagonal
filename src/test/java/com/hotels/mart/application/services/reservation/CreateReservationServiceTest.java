969cfa497df6089745810c1ed8d59d1e0dcb3c8e
Tito Nova <tnova@mysanitas.com>
2023-04-25 19:23:20 -0500

    test easy test

--- src/test/java/com/hotels/mart/application/services/reservation/CreateReservationServiceTest.java
+++ src/test/java/com/hotels/mart/application/services/reservation/CreateReservationServiceTest.java
@@ -1,11 +1,17 @@
 package com.hotels.mart.application.services.reservation;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class CreateReservationServiceTest {
     @Test
     void testCreateReservation() {
-        
+        boolean value1 = true;
+        boolean value2 = true;
+
+        assertTrue(value1 == value2, "Los valores deben ser iguales");
     }
+
 }
