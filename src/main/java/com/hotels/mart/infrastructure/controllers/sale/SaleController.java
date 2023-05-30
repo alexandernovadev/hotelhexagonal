2eb0e047c2171d41f3144bccc9d8fbdec2db9e26
dani101820 <111008893+dani101820@users.noreply.github.com>
2023-05-29 23:20:55 -0500

    cambios principales

--- src/main/java/com/hotels/mart/infrastructure/controllers/sale/SaleController.java
+++ src/main/java/com/hotels/mart/infrastructure/controllers/sale/SaleController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,11 @@ import com.hotels.mart.application.services.sales.CreateSaleService;
 import com.hotels.mart.application.services.sales.GetSaleByIdService;
 import com.hotels.mart.domain.entities.Sale;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/sales")
@@ -29,7 +33,10 @@ public class SaleController {
 
   @Autowired
   private CreateSaleService createSaleService;
-
+  @Operation(summary = "Busqueda de ventas por id", description = "Aqui se puede buscar por id las ventas realizadas", responses = {
+    @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(schema = @Schema(implementation = Sale.class))),
+    @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+    @ApiResponse(responseCode = "404", description = "Sales not found") })
   @GetMapping("/searchById")
   public ResponseEntity<?> serchById(@RequestParam(value = "sales_id", required = false) Long saleId) {
     log.info("Buscando Sale By Id");
