12964f58ff6bb7a233686aaeb854b0f02872a45b
bspena65 <bspena65@ucatolica.edu.co>
2023-06-05 13:21:40 -0500

    Migrtating SQL and dokcer file

---
+++ init.sql
@@ -0,0 +1,181 @@
+CREATE TABLE `users_state` (
+  `user_state_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `state` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`user_state_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `users` (
+  `user_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_type` VARCHAR(255),
+  `user_state_id` BIGINT,
+  `name` VARCHAR(255),
+  `email` VARCHAR(255),
+  `password` VARCHAR(255),
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`user_state_id`) REFERENCES `users_state`(`user_state_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `traceability` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255),
+  `eventname` VARCHAR(255),
+  `datenow` DATETIME,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `sales_state` (
+  `sales_state_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `state` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`sales_state_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `payment_methods` (
+  `payment_method_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `method_name` VARCHAR(255),
+  PRIMARY KEY (`payment_method_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `rooms_type` (
+  `rooms_type_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `state` VARCHAR(255),
+  PRIMARY KEY (`rooms_type_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `rooms_state` (
+  `rooms_state_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `state` VARCHAR(255),
+  PRIMARY KEY (`rooms_state_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `rooms` (
+  `room_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `type_room_id` BIGINT,
+  `state_room_id` BIGINT,
+  `name` VARCHAR(255),
+  `description` VARCHAR(255),
+  `cost` DECIMAL(10, 2) NOT NULL,
+  PRIMARY KEY (`room_id`),
+  FOREIGN KEY (`type_room_id`) REFERENCES `rooms_type`(`rooms_type_id`),
+  FOREIGN KEY (`state_room_id`) REFERENCES `rooms_state`(`rooms_state_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `reservations_state` (
+  `reservation_state_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `state` VARCHAR(255),
+  PRIMARY KEY (`reservation_state_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `reservations` (
+  `reservation_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT,
+  `room_id` BIGINT,
+  `reservation_state_id` BIGINT,
+  `check_in_date` DATETIME,
+  `check_out_date` DATETIME,
+  PRIMARY KEY (`reservation_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+  FOREIGN KEY (`room_id`) REFERENCES `rooms`(`room_id`),
+  FOREIGN KEY (`reservation_state_id`) REFERENCES `reservations_state`(`reservation_state_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `sales` (
+  `sales_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT,
+  `reservation_id` BIGINT,
+  `sales_state_id` BIGINT,
+  `payment_method_id` BIGINT,
+  `total_amount` DECIMAL(10, 2) NOT NULL,
+  `quantity` INT NOT NULL,
+  PRIMARY KEY (`sales_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+  FOREIGN KEY (`reservation_id`) REFERENCES `reservations`(`reservation_id`),
+  FOREIGN KEY (`sales_state_id`) REFERENCES `sales_state`(`sales_state_id`),
+  FOREIGN KEY (`payment_method_id`) REFERENCES `payment_methods`(`payment_method_id`)
+) ENGINE=InnoDB;
+
+-- Review Table
+
+CREATE TABLE `ratings` (
+  `rating_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `reservation_id` BIGINT,
+  `user_id` BIGINT,
+  `rating` INT,
+  `comment` VARCHAR(255),
+  PRIMARY KEY (`rating_id`),
+  FOREIGN KEY (`reservation_id`) REFERENCES `reservations`(`reservation_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
+) ENGINE=InnoDB;
+
+
+-- Insert into users_state
+INSERT INTO `users_state` (`state`) VALUES ('Active');
+INSERT INTO `users_state` (`state`) VALUES ('Inactive');
+INSERT INTO `users_state` (`state`) VALUES ('Suspended');
+INSERT INTO `users_state` (`state`) VALUES ('Banned');
+INSERT INTO `users_state` (`state`) VALUES ('Deleted');
+
+-- Insert into sales_state
+INSERT INTO `sales_state` (`state`) VALUES ('Pending');
+INSERT INTO `sales_state` (`state`) VALUES ('Completed');
+INSERT INTO `sales_state` (`state`) VALUES ('Cancelled');
+INSERT INTO `sales_state` (`state`) VALUES ('In Process');
+INSERT INTO `sales_state` (`state`) VALUES ('Failed');
+
+-- Insert into payment_methods
+INSERT INTO `payment_methods` (`method_name`) VALUES ('Credit Card');
+INSERT INTO `payment_methods` (`method_name`) VALUES ('Debit Card');
+INSERT INTO `payment_methods` (`method_name`) VALUES ('Paypal');
+INSERT INTO `payment_methods` (`method_name`) VALUES ('Bank Transfer');
+INSERT INTO `payment_methods` (`method_name`) VALUES ('Cash');
+
+-- Insert into rooms_type
+INSERT INTO `rooms_type` (`state`) VALUES ('Single');
+INSERT INTO `rooms_type` (`state`) VALUES ('Double');
+INSERT INTO `rooms_type` (`state`) VALUES ('Triple');
+INSERT INTO `rooms_type` (`state`) VALUES ('Quad');
+INSERT INTO `rooms_type` (`state`) VALUES ('Queen');
+
+-- Insert into rooms_state
+INSERT INTO `rooms_state` (`state`) VALUES ('Available');
+INSERT INTO `rooms_state` (`state`) VALUES ('Occupied');
+INSERT INTO `rooms_state` (`state`) VALUES ('Maintenance');
+INSERT INTO `rooms_state` (`state`) VALUES ('Cleaning');
+INSERT INTO `rooms_state` (`state`) VALUES ('Reserved');
+
+-- Insert into reservations_state
+INSERT INTO `reservations_state` (`state`) VALUES ('Booked');
+INSERT INTO `reservations_state` (`state`) VALUES ('Cancelled');
+INSERT INTO `reservations_state` (`state`) VALUES ('No-show');
+INSERT INTO `reservations_state` (`state`) VALUES ('Checked-in');
+INSERT INTO `reservations_state` (`state`) VALUES ('Checked-out');
+
+-- Insert into users (admin is the password)
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('admin', 1, 'Admin User', 'admin@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'John Doe', 'john.doe@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'Jane Smith', 'jane.smith@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'Robert Johnson', 'robert.johnson@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'Maria Garcia', 'maria.garcia@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('admin', 1, 'Super User', 'super.user@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'James Brown', 'james.brown@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'Emily Davis', 'emily.davis@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'Jessica Miller', 'jessica.miller@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'Michael Wilson', 'michael.wilson@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+INSERT INTO `users` (`user_type`, `user_state_id`, `name`, `email`, `password`) VALUES ('customer', 1, 'Sarah Moore', 'sarah.moore@example.com', '$2a$10$vQZyKo0NbHunzwiStvPfx.KCy6apDBkZBiOoWmgcE6AnlaDdaUD2S');
+
+-- Insert into rooms
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (1, 1, 'Room 101', 'Single room with park view', 80.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (2, 1, 'Room 102', 'Double room with sea view', 120.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (3, 1, 'Room 201', 'Triple room with city view', 150.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (4, 1, 'Room 202', 'Quad room with garden view', 200.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (5, 1, 'Room 301', 'Queen room with mountain view', 250.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (1, 1, 'Room 302', 'Single room with park view', 80.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (2, 1, 'Room 401', 'Double room with city view', 120.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (3, 1, 'Room 402', 'Triple room with garden view', 150.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (4, 1, 'Room 501', 'Quad room with sea view', 200.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (5, 1, 'Room 502', 'Queen room with city view', 250.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (1, 1, 'Room 601', 'Single room with mountain view', 80.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (2, 1, 'Room 602', 'Double room with park view', 120.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (3, 1, 'Room 701', 'Triple room with sea view', 150.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (4, 1, 'Room 702', 'Quad room with city view', 200.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (5, 1, 'Room 801', 'Queen room with garden view', 250.00);
+INSERT INTO `rooms` (`type_room_id`, `state_room_id`, `name`, `description`, `cost`) VALUES (1, 1, 'Room 802', 'Single room with sea view', 80.00);
