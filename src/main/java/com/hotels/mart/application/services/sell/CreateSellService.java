953311d6df68a9a90014cf97a6776362c3394e77
Alexander.Nova <Alexander.Nova@Contractor.Prometric.com>
2024-01-19 13:56:03 -0500

    Add some comment about how to begin with that

--- src/main/java/com/hotels/mart/application/services/sell/CreateSellService.java
+++ src/main/java/com/hotels/mart/application/services/sell/CreateSellService.java
@@ -4,14 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hotels.mart.domain.entities.Sell;
+import com.hotels.mart.infrastructure.jpa.repositories.BookRepository;
+import com.hotels.mart.infrastructure.jpa.repositories.CardMembershipRepository;
 import com.hotels.mart.infrastructure.jpa.repositories.SellRepository;
+import com.hotels.mart.infrastructure.jpa.repositories.UserRepository;
 
 @Service
 public class CreateSellService {
   @Autowired
   private SellRepository sellRepository;
 
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private BookRepository bookRepository;
+
+  @Autowired
+  private CardMembershipRepository cardmembershipRepository;
+
   public void createSell(Sell sell) {
+    // Verify if user exist
+    // Verify if books are avaliables
+    // Calculate all balance sum all books and verify it
+
+    // Veriry if user has memership
+       // Verify if user has balance
+
+    // If all is valid, save sell 
+
     sellRepository.save(sell);
   }
 
---
+++ src/main/java/com/hotels/mart/infrastructure/controllers/sell/SellController.java
@@ -0,0 +1,13 @@
+package com.hotels.mart.infrastructure.controllers.sell;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/sells")
+@Slf4j
+public class SellController {
+  
+}
